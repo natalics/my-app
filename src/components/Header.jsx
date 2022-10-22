485dadba4c9cb1b7038f69607b889b6f918c1b41
Natalya Galeeva <natalyagaleeva8@gmail.com>
2022-10-22 22:40:29 +0500

    changed img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import s from "./Header.module.css";
 const Header = () => {
   return (
     <header className={s.header}>
-      <img src="https://static.vecteezy.com/system/resources/previews/011/318/152/non_2x/explore-text-hand-drawn-illustration-design-png.png" />
+      <img src="https://psv4.userapi.com/c236331/u295814816/docs/d20/9388fd520835/logo.png?extra=JoPkt7ps2h0HTxDYXbrdgitRY_lJkGSsjLbfL9RiNRaIhGkrmdiEwh3IIddpNanCwYRslHMpXwyqf3uFPvHOFffvVE1vDfnKJouZuyp_iu62Y9FWtNiIXO37T_PhqPVgu90aYLkVANDdIuBpHjz8Y6w"/>
     </header>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,11 +5,11 @@ const Profile = () => {
   return (
     <div className={s.content}>
       <div>
-        <img src="" />
+        <img src="https://static.vecteezy.com/system/resources/previews/005/431/039/non_2x/paper-cut-background-abstract-realistic-papercut-decoration-textured-with-wavy-layers-3d-topography-relief-topographic-illustration-cover-layout-template-free-vector.jpg" />
       </div>
       <div>
         Avatar + description
-        <img src="https://cdn-icons-png.flaticon.com/512/4825/4825082.png" />
+        <img src="https://psv4.userapi.com/c236331/u295814816/docs/d38/57cd943125df/avatar.png?extra=GQ78mZPMYVPryCOAaVlCAX6iJvkaSg0_ZkMz6TdKCQ-eTLw6h-6vZGRP6bd6Ul7h3GT9tHkEAmSzYUfIhSnkwYB1Wv4WiIHDDLTETqDbls_QQr8ee5UkuQqiiae0y2Ps1XCJ5HatvP_vts7VcZZU2SM" />
       </div>
       <div>
         My posts
